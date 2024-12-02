f7375b365dc0adecca9438ae44e89f1e7f87ee1e
floydremmerswaal <floydremmerswaal@gmail.com>
2024-12-02 20:00:47 +0100

    fix racket implementation

--- 2024/day2/solution.rkt
+++ 2024/day2/solution.rkt
@@ -1,23 +1,59 @@
 #lang racket
 
-(require megaparsack megaparsack/text)
-
-(define (isMonotoneIncreasing lst)
-  (apply < lst))
-
-(define (isMonotoneDecreasing lst)
-  (apply > lst))
-
-(define (isMonotone lst)
-  (or (isMonotoneIncreasing lst) (isMonotoneDecreasing lst)))
 
 (define (mapToInts lst)
   (map string->number lst))
 
+(define (allPositive lst)
+  (for/and ([x lst])
+    (positive? x)))
+
+(define (allNegative lst)
+  (for/and ([x lst])
+    (negative? x)))
+
+; create a new list with the differences between each element
+
+(define (differenceHelper lst acc)
+  (if (null? (cdr lst))
+      acc
+      (differenceHelper (cdr lst) (cons (- (car lst) (cadr lst)) acc))))
+
+(define (differences lst)
+  (reverse (differenceHelper lst '())))
+
+(define (toPositive lst)
+  (map abs lst))
+
+(define (isMonotone lst)
+  (or (allPositive (differences lst) )
+      (allNegative (differences lst))))
+
+
+(define (allBetween1And3 lst)
+  (for/and ([x lst])
+    (and (<= 1 x) (<= x 3))))
+
+(define (isSafe lst)
+  (and (isMonotone lst) (allBetween1And3 (toPositive (differences lst)))))
+
 (define (processLine line) 
-  (isMonotone (mapToInts (string-split line " "))))
+  (isSafe (mapToInts (string-split line " "))))
 
+(define (true? x)
+  (eq? x #t))
 
-(for ([line (file->lines "example.txt")])
-  ;process line and count the number of "true" results  
-  (print (processLine line)))
+; construct all possible sublists (with 1 element missing)
+; and check if any of them are safe
+(define (isSafe2 lst)
+  (ormap isSafe
+         (for/list ([i (in-range (length lst))])
+           (append (take lst i) (drop lst (add1 i))))))
+
+(define (processLine2 line) 
+  (isSafe2 (mapToInts (string-split line " "))))
+
+(count true? (map processLine (file->lines "input.txt")))
+
+(count true? (map processLine2 (file->lines "input.txt")))
+  
